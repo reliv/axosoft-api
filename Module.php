70a01bc6a9a9fa967eda148e60f558a84e9d1f04
James Jervis <jjervis@relivinc.com>
2015-04-24 12:15:47 -0500

    Fix Module loading issue

--- Module.php
+++ Module.php
@@ -1,40 +1,6 @@
 <?php
-
 /**
- * Module Config For ZF2
- */
-
-namespace Reliv\AxosoftApi;
-
-class Module
-{
-    /**
-     * getAutoloaderConfig() is a requirement for all Modules in ZF2.  This
-     * function is included as part of that standard.  See Docs on ZF2 for more
-     * information.
-     *
-     * @return array Returns array to be used by the ZF2 Module Manager
-     */
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src',
-                ),
-            ),
-        );
-    }
-
-    /**
-     * getConfig() is a requirement for all Modules in ZF2.  This
-     * function is included as part of that standard.  See Docs on ZF2 for more
-     * information.
-     *
-     * @return array Returns array to be used by the ZF2 Module Manager
-     */
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-}
+* This file is placed here for compatibility with Zendframework 2's ModuleManager.
+* It allows usage of this module even without composer.
+*/
+require_once __DIR__ . '/src/Module.php';
